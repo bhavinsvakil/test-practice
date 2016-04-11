a896421b323ada1e2bdb185365d3ff11d7400cd6
bhavinsvakil <bhavinsvakil@gmail.com>
2016-04-11 12:04:29 -0400

    my initial commit

--- spec/login_orchard.rb
+++ spec/login_orchard.rb
@@ -16,7 +16,6 @@ require 'byebug'
       @driver.find_element(name: 'username').send_keys('bvakil')
       @driver.find_element(name: 'password').send_keys('bhavin@123')
       @driver.find_element(id: 'sign_in').submit
-      byebug
       user_name= @driver.find_element(id: 'logged_in_user').text
       expect(user_name). to include ("Bhavin")
       expect(@driver.current_url). to eql ('http://oa.qaorch.com/relationships.php')
